3d9b0520e3bb8007ff5b5a31a3397ec04f688c4a
Dana Mastropole <Dana@Danas-MacBook-Pro-2.local>
2016-11-25 16:34:53 -0500

    add allowed extensions

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ from flask import Flask, render_template, request, redirect, send_file
 from werkzeug import secure_filename
 from os import system
 
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
 app = Flask(__name__)
 
 @app.route('/', methods=['GET','POST'])
@@ -13,10 +18,11 @@ def upload_file1():
 
 @app.route('/img', methods = ['GET', 'POST'])
 def upload_file2():
-   if request.method == 'POST':
-      f = request.files['file']
-      f.save('uploaded_image')
-      return send_file('uploaded_image', mimetype='image/gif')
+    if request.method == 'POST':
+        f = request.files['file']
+        if allowed_file(f.filename.lower()):
+            f.save('uploaded_image')
+            return send_file('uploaded_image', mimetype='image/gif')
 		
 if __name__ == '__main__':
    app.run(debug = True)
