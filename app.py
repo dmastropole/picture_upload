9f7dab6b6c5636a5427a188a705d13e0f1175849
Dana Mastropole <Dana@Danas-MacBook-Pro-2.local>
2016-11-25 17:08:59 -0500

    specify port

--- app.py
+++ app.py
@@ -25,5 +25,6 @@ def upload_file2():
             return send_file('uploaded_image', mimetype='image/gif')
 		
 if __name__ == '__main__':
-   app.run(debug = True)
+   port = int(os.environ.get("PORT", 5000))
+   app.run(debug=True, host='0.0.0.0', port=port)
    system('rm uploaded_image')
