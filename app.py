bbf89bf75d7a474bcfacc4c1481ac3a528fa1867
Dana Mastropole <Dana@Danas-MacBook-Pro-2.local>
2016-11-25 17:11:24 -0500

    fix bug

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, send_file
 from werkzeug import secure_filename
-from os import system
+from os import system, environ
 
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 def allowed_file(filename):
@@ -25,6 +25,6 @@ def upload_file2():
             return send_file('uploaded_image', mimetype='image/gif')
 		
 if __name__ == '__main__':
-   port = int(os.environ.get("PORT", 5000))
+   port = int(environ.get("PORT", 5000))
    app.run(debug=True, host='0.0.0.0', port=port)
    system('rm uploaded_image')
